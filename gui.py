bafa5e63fcd04d503598bd6bbb292b614ec03d54
Dove6 <dawidsygocki1234@wp.pl>
2019-12-26 16:32:06 +0100

    Improve gui.py
    Make window appear showing dummy widgets.

--- gui.py
+++ gui.py
@@ -1,6 +1,25 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QLabel, QTableView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QLabel, QTableView, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
-from PyQt5.QtCharts import QChartView
+from PyQt5.QtChart import QChart, QChartView, QBarSet, QBarCategoryAxis, QLineSeries
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        vbox_layout = QVBoxLayout()
+        combo = QComboBox()
+        vbox_layout.addWidget(combo)
+        chart = QChartView()
+        vbox_layout.addWidget(chart)
+        table = QTableView()
+        vbox_layout.addWidget(table)
+        main_widget = QWidget()
+        main_widget.setLayout(vbox_layout)
+        self.setCentralWidget(main_widget)
+
+class Popup(QWidget):
+    pass
+
 
 # Enable support for hi-dpi screens (https://leomoon.com/journal/python/high-dpi-scaling-in-pyqt5/)
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
@@ -8,4 +27,6 @@ QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 application = QApplication([])
 
-
+window = MainWindow()
+window.show()
+application.exec_()
