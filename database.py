abdbc49c42671b64c4c12a95795f5e43a5d2de4f
Dove6 <dawidsygocki1234@wp.pl>
2019-12-29 20:10:20 +0100

    Improve GUI and SQL queries, create database connection manager
    GUI: prepare graph data interface, reorganise MainWindow class.
    SQL: restructure queries according to data format specified in
    previous commit, create SQLite context manager using built-in
    Python context management mechanics.

--- database.py
+++ database.py
@@ -1,21 +1,37 @@
 import sqlite3
 
 
-connection = sqlite3.connect('cache.db')
-cursor = connection.cursor()
+class SQLite3Cursor:
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+    def __enter__(self):
+        self._connection = sqlite3.connect(*self._args, *self._kwargs)
+        return self._connection.cursor()
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type is None and exc_value is None and exc_traceback is None:
+            self._connection.commit()
+        else:
+            self._connection.rollback()
+        self._connection.close()
+        return False
+
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS offices (
-        id TEXT NOT NULL PRIMARY KEY,
-        name TEXT
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        key TEXT
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS matters (
-        id INTEGER NOT NULL PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name TEXT,
-        office_id TEXT NOT NULL,
+        ordinal INT,
+        group_id INT,
+        office_id INTEGER NOT NULL,
         FOREIGN KEY (office_id)
             REFERENCES offices (id)
     )
@@ -36,27 +52,52 @@ cursor.execute('''
 cursor.commit()
 
 cursor.execute('''
-    DELETE FROM samples
-    WHERE DATETIME(time) < DATETIME('now', 'localtime', '-1 hour')
+    SELECT *
+    FROM offices
+    ORDER BY name
 ''')
 
 cursor.execute('''
-    SELECT *
-    FROM offices
-''')
+    INSERT INTO offices (name, key)
+    VALUES (?, ?)
+''', (str, str))
+
 
 cursor.execute('''
     SELECT *
     FROM matters
-''')
+    WHERE office_id = ?
+    ORDER BY name
+''', (int, ))
 
 cursor.execute('''
-    SELECT *
+    INSERT INTO offices (name, ordinal, group_id, office_id)
+    VALUES (?, ?, ?, ?)
+''', (str, int/None, int, int))
+
+
+cursor.execute('''
+    SELECT MAX(time)
     FROM samples
-''')
+    WHERE matter_id = ?
+''', (int, ))
 
 cursor.execute('''
-    INSERT INTO 
-''')
+    SELECT (STRFTIME('%s', ?) - STRFTIME('%s', time)) / 60 AS index, 
+        open_counters, queue_length
+    FROM samples
+    WHERE matter_id = ?
+    ORDER BY index
+''', (str, int))
+
+cursor.execute('''
+    DELETE FROM samples
+    WHERE DATETIME(time) < DATETIME(?, '-1 hour')
+''', (str, ))
+
+cursor.execute('''
+    INSERT INTO samples
+    VALUES (?, ?, ?, ?)
+''', (str, int, int, int))
 
 connection.close()
--- gui.py
+++ gui.py
@@ -1,21 +1,67 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QLabel, QTableView, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QLabel, QTableWidget, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
-from PyQt5.QtChart import QChart, QChartView, QBarSet, QBarCategoryAxis, QLineSeries
+from PyQt5.QtChart import QChart, QChartView, QLineSeries
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        vbox_layout = QVBoxLayout()
-        combo = QComboBox()
-        vbox_layout.addWidget(combo)
-        chart = QChartView()
-        vbox_layout.addWidget(chart)
-        table = QTableView()
-        vbox_layout.addWidget(table)
-        main_widget = QWidget()
-        main_widget.setLayout(vbox_layout)
-        self.setCentralWidget(main_widget)
+
+        self.resize(750, 550)
+
+        self._combo_list = []
+        self._chart_series = []
+
+        self._combo = QComboBox()
+
+        self._chart = QChart()
+        self._chart.setTitle('Tytuł')
+        self._chart.legend().setVisible(False)
+        self._chart_view = QChartView()
+        self._chart_view.setChart(self._chart)
+
+        self._table = QTableWidget()
+
+        self._vbox_layout = QVBoxLayout()
+        self._vbox_layout.addWidget(self._combo)
+        self._vbox_layout.addWidget(self._chart_view)
+        self._vbox_layout.addWidget(self._table)
+
+        self._main_widget = QWidget()
+        self._main_widget.setLayout(self._vbox_layout)
+        self.setCentralWidget(self._main_widget)
+
+    def _update_chart(self):
+        self._chart.removeAllSeries()
+        for series in self._chart_series:
+            line_series = QLineSeries()
+            for point in series:
+                line_series.append(*point)
+            self._chart.addSeries(line_series)
+        self._chart.createDefaultAxes()
+
+    @property
+    def chart_series(self):
+        return self._chart_series[:]
+
+    @chart_series.setter
+    def chart_series(self, value):
+        self._chart_series = value[:]
+        self._update_chart()
+
+    def _update_combo(self):
+        self._combo.clear()
+        self._combo.addItems(self._combo_list)
+
+    @property
+    def combo_list(self):
+        return self._combo_list[:]
+
+    @combo_list.setter
+    def combo_list(self, value):
+        self._combo_list = value[:]
+        self._update_combo()
+
 
 class Popup(QWidget):
     pass
